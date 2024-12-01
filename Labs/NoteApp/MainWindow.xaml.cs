6faaa9ca4bd86d73e5b797d09ceb06803cd128f4
Vanchik21 <ipz221_zia@student.ztu.edu.ua>
2024-12-01 18:04:36 +0200

    NoteApp v1.1

--- Labs/NoteApp/MainWindow.xaml.cs
+++ Labs/NoteApp/MainWindow.xaml.cs
@@ -3,89 +3,122 @@ using System.IO;
 using System.Windows;
 using System.Windows.Documents;
 using System.Windows.Media;
+using System.Windows.Threading;
 
-namespace NoteApp;
-
-public partial class MainWindow : Window
+namespace NoteApp
 {
-    public MainWindow()
+    public partial class MainWindow : Window
     {
-        InitializeComponent();
-    }
-
-    private void NewFile_Click(object sender, RoutedEventArgs e)
-    {
-        Editor.Document.Blocks.Clear();
-    }
-
-    private void OpenFile_Click(object sender, RoutedEventArgs e)
-    {
-        OpenFileDialog openFileDialog = new OpenFileDialog
+        public MainWindow()
         {
-            Filter = "Rich Text Format (*.rtf)|*.rtf"
-        };
-        if (openFileDialog.ShowDialog() == true)
+            InitializeComponent();
+            StartAutoSave();
+        }
+
+        private void NewFile_Click(object sender, RoutedEventArgs e)
         {
-            TextRange range = new TextRange(Editor.Document.ContentStart, Editor.Document.ContentEnd);
-            using (FileStream fs = new FileStream(openFileDialog.FileName, FileMode.Open))
+            Editor.Document.Blocks.Clear();
+        }
+
+        private void OpenFile_Click(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog
             {
-                range.Load(fs, DataFormats.Rtf);
+                Filter = "Rich Text Format (*.rtf)|*.rtf"
+            };
+            if (openFileDialog.ShowDialog() == true)
+            {
+                TextRange range = new TextRange(Editor.Document.ContentStart, Editor.Document.ContentEnd);
+                using (FileStream fs = new FileStream(openFileDialog.FileName, FileMode.Open))
+                {
+                    range.Load(fs, DataFormats.Rtf);
+                }
             }
         }
-    }
 
-    private void SaveFile_Click(object sender, RoutedEventArgs e)
-    {
-        SaveFileDialog saveFileDialog = new SaveFileDialog
+        private void SaveFile_Click(object sender, RoutedEventArgs e)
         {
-            Filter = "Rich Text Format (*.rtf)|*.rtf"
-        };
-        if (saveFileDialog.ShowDialog() == true)
+            SaveFileDialog saveFileDialog = new SaveFileDialog
+            {
+                Filter = "Rich Text Format (*.rtf)|*.rtf"
+            };
+            if (saveFileDialog.ShowDialog() == true)
+            {
+                TextRange range = new TextRange(Editor.Document.ContentStart, Editor.Document.ContentEnd);
+                using (FileStream fs = new FileStream(saveFileDialog.FileName, FileMode.Create))
+                {
+                    range.Save(fs, DataFormats.Rtf);
+                }
+            }
+        }
+
+        private void Exit_Click(object sender, RoutedEventArgs e)
         {
+            Application.Current.Shutdown();
+        }
+
+        private void FindSymbol_Click(object sender, RoutedEventArgs e)
+        {
+            string symbol = Microsoft.VisualBasic.Interaction.InputBox("Enter the symbol to find:", "Find Symbol");
+            TextRange documentRange = new TextRange(Editor.Document.ContentStart, Editor.Document.ContentEnd);
+            documentRange.ApplyPropertyValue(TextElement.BackgroundProperty, Brushes.Transparent);
+
+            if (!string.IsNullOrEmpty(symbol) && symbol.Length == 1)
+            {
+                string text = new TextRange(Editor.Document.ContentStart, Editor.Document.ContentEnd).Text;
+                int index = 0;
+                while ((index = text.IndexOf(symbol, index)) != -1)
+                {
+                    TextPointer start = GetTextPointerAtOffset(Editor.Document.ContentStart, index);
+                    TextPointer end = GetTextPointerAtOffset(Editor.Document.ContentStart, index + 1);
+                    if (start != null && end != null)
+                    {
+                        new TextRange(start, end).ApplyPropertyValue(TextElement.BackgroundProperty, Brushes.Yellow);
+                    }
+                    index++;
+                }
+            }
+        }
+
+        private TextPointer GetTextPointerAtOffset(TextPointer start, int offset)
+        {
+            while (offset > 0 && start != null)
+            {
+                if (start.GetPointerContext(LogicalDirection.Forward) == TextPointerContext.Text)
+                {
+                    int count = start.GetTextRunLength(LogicalDirection.Forward);
+                    if (offset <= count) return start.GetPositionAtOffset(offset);
+                    offset -= count;
+                }
+                start = start.GetPositionAtOffset(1, LogicalDirection.Forward);
+            }
+            return start;
+        }
+
+        private void About_Click(object sender, RoutedEventArgs e)
+        {
+            AboutBox aboutBox = new AboutBox();
+            aboutBox.ShowDialog();
+        }
+
+        private void AutoSave()
+        {
+            string backupPath = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData), "NoteAppBackup.rtf");
             TextRange range = new TextRange(Editor.Document.ContentStart, Editor.Document.ContentEnd);
-            using (FileStream fs = new FileStream(saveFileDialog.FileName, FileMode.Create))
+            using (FileStream fs = new FileStream(backupPath, FileMode.Create))
             {
                 range.Save(fs, DataFormats.Rtf);
             }
         }
-    }
 
-    private void Exit_Click(object sender, RoutedEventArgs e)
-    {
-        Application.Current.Shutdown();
-    }
-
-    private void FindSymbol_Click(object sender, RoutedEventArgs e)
-    {
-        string symbol = Microsoft.VisualBasic.Interaction.InputBox("Enter the symbol to find:", "Find Symbol");
-        TextRange documentRange = new TextRange(Editor.Document.ContentStart, Editor.Document.ContentEnd);
-        documentRange.ApplyPropertyValue(TextElement.BackgroundProperty, Brushes.Transparent);
-
-        if (!string.IsNullOrEmpty(symbol) && symbol.Length == 1)
+        private void StartAutoSave()
         {
-            char searchChar = symbol[0];
-            TextPointer pointer = Editor.Document.ContentStart;
-
-            while (pointer != null)
+            DispatcherTimer timer = new DispatcherTimer
             {
-                TextPointer nextPointer = pointer.GetPositionAtOffset(1, LogicalDirection.Forward);
-                if (nextPointer == null) break;
-
-                string textInRange = new TextRange(pointer, nextPointer).Text;
-                if (textInRange == symbol)
-                {
-                    TextRange highlightRange = new TextRange(pointer, nextPointer);
-                    highlightRange.ApplyPropertyValue(TextElement.BackgroundProperty, Brushes.Yellow);
-                }
-
-                pointer = nextPointer;
-            }
+                Interval = TimeSpan.FromMinutes(1)
+            };
+            timer.Tick += (s, e) => AutoSave();
+            timer.Start();
         }
-    }
-
-    private void About_Click(object sender, RoutedEventArgs e)
-    {
-        AboutBox aboutBox = new AboutBox();
-        aboutBox.ShowDialog();
     }
 }
